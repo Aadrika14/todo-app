5b6c7bcf751cb074e87e32458cd4624d6ec2636d
aadrika <144731869+Aadrika14@users.noreply.github.com>
2024-05-08 17:49:19 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
+
 import App from './App.jsx';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router } from 'react-router-dom';
 
-ReactDOM.render(
+createRoot(document.getElementById('root')).render( // Use createRoot instead of ReactDOM.render
   <React.StrictMode>
     <Router>
       <App />
     </Router>
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
